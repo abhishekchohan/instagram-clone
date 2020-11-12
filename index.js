3181255c5fcb3148cd81a0e3530f9f7b8b6aa151
Abhishek Chohan <abhishekchohan1@gmail.com>
2020-11-11 21:22:58 -0500

    port npm start script changed

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-require('dotenv').config()
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const PORT = 1234 || process.env.PORT;
+const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => console.log("Server is running on " + PORT));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-mongoose.connection.on('connected', () => {
-    console.log("Connected to DB :)");
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to DB :)");
+});
 
-require('./models/user');
-require('./models/post');
-app.use(require('./routes/auth'));
-app.use(require('./routes/post'));
+require("./models/user");
+require("./models/post");
+app.use(require("./routes/auth"));
+app.use(require("./routes/post"));
